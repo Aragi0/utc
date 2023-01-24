f63fe5b107c0207e960260aad0639db55aa527c0
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-01-23 23:49:37 -0600

    css
    se cambio el modo black por el wild para que no nos queme los ojos y se corrigo un bug en la tabla que acia que no se mostrara partes en el modo movile

--- resources/views/altas/altaMaestro.blade.php
+++ resources/views/altas/altaMaestro.blade.php
@@ -3,7 +3,7 @@
 <!-- A partir de aqui se mostrara el modulo de alta de maestros-->
 
 <div class="recent-orders">
-    <h2>Tabla de Alumnos</h2>
+    <h2>Maestros</h2>
     <table>
         <thead>
             <tr>
@@ -19,6 +19,33 @@
                 <td>Francisco Javier</td>
                 <td>Tamay Canul</td>
                 <td>9881090497</td>
+                <td></td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr>
+                <td>626131</td>
+                <td>Oscar Aldair</td>
+                <td>Matu Miranda</td>
+                <td>9881090237</td>
+                <td></td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr>
+                <td>62626</td>
+                <td>David Asael</td>
+                <td>Tamay Canul</td>
+                <td>913123197</td>
+                <td></td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr>
+                <td>12323</td>
+                <td>Hector Eduardo</td>
+                <td>cam</td>
+                <td>9292739192</td>
                 <td></td>
             </tr>
         </tbody>
