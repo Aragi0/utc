8b0cadd877007d40ce2433f2b1363aa37af16db7
Aragi <119747217+Aragikuu@users.noreply.github.com>
2023-02-02 10:00:32 -0600

    vista modal

--- resources/views/altas/vistamodal.blade.php
+++ resources/views/altas/vistamodal.blade.php
@@ -3,23 +3,23 @@
 <!--copean too y lo ponen donde lo quieran usar-->
 <!--este bottom lo ponen donde quieren que se abra la ventana-->
 <div class="boton-modal">
-    <label for="btn-modal"><!--el contenido (Abrir modal)es relatvo y pueden cambirlo por cualqier cosa --> Abrir Modal </label>
+  <label for="btn-modal"><!--el contenido (Abrir modal)es relatvo y pueden cambirlo por cualqier cosa --> Abrir Modal </label>
 </div>
 <!--Fin de Boton-->
 <!--Ventana Modal-->
 <input type="checkbox" id="btn-modal" />
 <div class="container-modal">
-    <div class="content-modal">
-    <table class="table table-striped">
-    <thead class="thead-dark">
-          <th>SKU</th>
-          <th>PRODUCTO</th>
-          <th>OPERACION</th>
-          <th>PRECIO</th>
-          <th>CANTIDAD</th>
-          <th>TOTAL</th>
-        </thead>
-          
+  <div class="content-modal">
+    <table>
+      <thead>
+        <th>SKU</th>
+        <th>PRODUCTO</th>
+        <th>OPERACION</th>
+        <th>PRECIO</th>
+        <th>CANTIDAD</th>
+        <th>TOTAL</th>
+      </thead>
+
       <tbody>
         <tr>
           <td></td>
@@ -31,12 +31,12 @@
         </tr>
       </tbody>
     </table>
-        <div class="btn-cerrar">
-            <label for="btn-modal">Cerrar</label>
-        </div>
-        <!--no pongan contenido fuera de este div puesto que las propiedades se apliacin adentro-->
+    <div class="btn-cerrar">
+      <label for="btn-modal">Cerrar</label>
     </div>
-    <label for="btn-modal" class="cerrar-modal"></label>
+    <!--no pongan contenido fuera de este div puesto que las propiedades se apliacin adentro-->
+  </div>
+  <label for="btn-modal" class="cerrar-modal"></label>
 </div>
 
 @endsection
