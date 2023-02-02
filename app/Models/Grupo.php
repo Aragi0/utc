f45488b310fde2ab05676a2b65596cdc8217731f
FranciscoTamay <91625507+FranciscoTamay@users.noreply.github.com>
2023-02-01 18:08:20 -0600

    modelos
    sube el tuyo david

---
+++ app/Models/Grupo.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Grupo extends Model
+{
+    use HasFactory;
+
+    protected $table='grupos';
+
+    protected $primaryKey='id';
+
+    protected $whit=['matriculas','asignaturas','maestros','carreras'];
+
+    public $incrementing=false;
+
+    public $timestamps=false;
+
+    protected $fillable=[
+        'id',
+        'id_matricula',
+        'id_asignatura',
+        'id_profesor',
+        'carrera_id'
+    ];
+
+    public function matriculas(){
+        return $this->belongsTo(Matriculas::class, 'id_matricula');
+    }
+
+    public function asigatura(){
+        return $this->belongsTo(Asignaturas::class, 'id_asignaturas');
+    }
+
+    public function maestros(){
+        return $this->belongsTo(Matriculas::class, 'id_matricula');
+    }
+}
