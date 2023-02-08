2be20eda2db5e80059a7ea42bd7650d0b2fd919d
FranciscoTamay <91625507+FranciscoTamay@users.noreply.github.com>
2023-02-08 17:00:33 -0600

    modelo plan de estudio
    no le muevas david

--- app/Models/Plan_Estudio.php
+++ app/Models/Plan_Estudio.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Asignaturas;
+use mysqli;
 
 class Plan_Estudio extends Model
 {
     use HasFactory;
+
+    protected $table='plan';
+
+    protected $primaryKey='id';
+
+    protected $whit=['plan_estudio'];
+
+    public $incrementing=false;
+
+    public $timestamps=false;
+
+    protected $fillable=[
+        'id',
+        'id_asignatura',
+        'nombre_plan',
+        'anio',
+        'cuatrimestres',
+        'horas'
+    ];
+
+    public function asginatura(){
+        return $this->belongsTo(Asignaturas::class, 'id_plan');
+    }
 }
